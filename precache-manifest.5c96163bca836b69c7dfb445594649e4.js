dfeb1277ea0337503d2577827a069d1e7b28f5a0
bpankaj2206 <bpankaj2206@gmail.com>
2021-03-26 13:29:30 +0530

    Updates

---
+++ precache-manifest.5c96163bca836b69c7dfb445594649e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3178bba7e80abcae778e4f4ff756f6d0",
+    "url": "/recipefinder/index.html"
+  },
+  {
+    "revision": "ce4d8f8f2f327388b83a",
+    "url": "/recipefinder/static/css/main.a778cd76.chunk.css"
+  },
+  {
+    "revision": "3426c049d1172a2d43a4",
+    "url": "/recipefinder/static/js/2.1d76c5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipefinder/static/js/2.1d76c5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4d8f8f2f327388b83a",
+    "url": "/recipefinder/static/js/main.77e6c753.chunk.js"
+  },
+  {
+    "revision": "3198f6aa2cd06d94c3c0",
+    "url": "/recipefinder/static/js/runtime-main.0195cfe0.js"
+  }
+]);
